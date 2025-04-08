9ed2f801fed27d91381d78f7a23ca44f23c85338
nisar512 <nisar.51297@gmail.com>
2025-04-08 19:02:35 +0500

    backend starter

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    POSTGRES_URL: str = "postgresql+asyncpg://user:password@db:5432/dbname"
+    ELASTICSEARCH_URL: str = "http://elasticsearch:9200"
+    SELENIUM_REMOTE_URL: str = "http://localhost:4444/wd/hub"
+    JWT_SECRET: str = ""
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
